f1a26a2b6cb164531b5411c1a6ec539084f5e4bf
Ilias Dinia <iliasd752@gmail.com>
2020-09-24 19:24:23 +0200

    AD-19 AD-20 AD-17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <div>
+      <ngx-datatable class="material" [rows]="rows" [columns]="columns"> </ngx-datatable>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'test3';
+  rows = [
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Molly', gender: 'Female', company: 'Burger King' }
+  ];
+  columns = [{ prop: 'name' }, { name: 'Gender' }, { name: 'Company' }];
 }
